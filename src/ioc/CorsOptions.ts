cd3515c353082108979e517317671ecd530733b5
AnderDev009 <anderson_ortiz05@hotmail.com>
2023-11-26 18:32:36 -0500

    Ultimas configuraciones x3

--- src/ioc/CorsOptions.ts
+++ src/ioc/CorsOptions.ts
@@ -1,6 +1,6 @@
 class CorsOptionsDependency {
     static CorsOptions = {
-        origin: 'http://localhost:3000', // Sustituye variables
+        origin: 'https://nextjs-finalprogweb.vercel.app/', // Sustituye variables
         methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
         credentials: true,
         optionsSuccessStatus: 200,
